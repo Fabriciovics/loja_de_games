f2e46a8a8b979e107c308a8907e8cab9b6b0da73
Fabriciovics <fabriciovics@gmail.com>
2025-05-21 18:39:52 -0300

    Adicao dos Query Methods lessThan e GreaterThan

--- src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
@@ -3,8 +3,11 @@ package com.generation.lojadegames.repository;
 import com.generation.lojadegames.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
         List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+        List<Produto> findByPrecoLessThan(BigDecimal preco);
+        List<Produto> findByPrecoGreaterThan(BigDecimal preco);
 }
